a6b188a2d0419f524bfa5c20cee306e76eb4d3cc
Achim Gädke <achim.gaedke@signal41.com>
2016-04-14 09:24:31 +1200

    start using interface functions in merlin.Process methods

--- pymerlin/processes.py
+++ pymerlin/processes.py
@@ -10,18 +10,16 @@ class BudgetProcess(merlin.Process):
         p_output = merlin.ProcessOutput('output_$', '$', connector=None)
 
         # Define properties
-        p_property = merlin.ProcessProperty(
-            'amount',
-            property_type=merlin.ProcessProperty.PropertyType.number_type,
-            default=start_amount,
-            parent=self)
+        self.add_property("amount",
+                          "amount",
+                          merlin.ProcessProperty.PropertyType.number_type,
+                          start_amount)
 
         self.outputs = {'$': p_output}
-        self.props = {'amount': p_property}
 
     def reset(self):
         # define internal instance variables on init
-        self.current_amount = self.props['amount'].get_value()
+        self.current_amount = self.get_prop_value("amount")
         self.amount_per_step = self.current_amount / self.parent.sim.num_steps
 
     def compute(self, tick):
@@ -30,9 +28,9 @@ class BudgetProcess(merlin.Process):
             if output > self.current_amount:
                 output = self.current_amount
             self.current_amount -= output
-            self.outputs['$'].connector.write(output)
+            self.provide_output('$', output)
         else:
-            self.outputs['$'].connector.write(0.00)
+            self.provide_output('$', 0.0)
 
 
 class CallCenterStaffProcess(merlin.Process):
@@ -43,70 +41,60 @@ class CallCenterStaffProcess(merlin.Process):
         # Define inputs
         i_desks = merlin.ProcessInput('i_desks', 'desks', connector=None)
         i_funds = merlin.ProcessInput('i_$', '$', connector=None)
+        self.inputs = {'desks': i_desks, '$': i_funds}
 
         # Define outputs
         o_requests_handled = merlin.ProcessOutput(
             'o_requests_handled',
             'requests_handled',
             connector=None)
+        self.outputs = {'requests_handled': o_requests_handled}
 
         # Define properties
-        p_staff = merlin.ProcessProperty(
+        self.add_property(
+            'staff number',
             'staff number',
             property_type=merlin.ProcessProperty.PropertyType.number_type,
-            default=100,
-            parent=self)
+            default_value=100)
 
-        p_staff_salary = merlin.ProcessProperty(
+        self.add_property(
+            'staff salary',
             'staff salary',
             property_type=merlin.ProcessProperty.PropertyType.number_type,
-            default=5.00,
-            parent=self)
+            default_value=5.00)
 
-        p_staff_per_desk = merlin.ProcessProperty(
+        self.add_property(
+            'staff per desk',
             'staff per desk',
             property_type=merlin.ProcessProperty.PropertyType.number_type,
-            default=1,
-            parent=self)
+            default_value=1)
 
-        p_months_to_train = merlin.ProcessProperty(
+        self.add_property(
+            'months to train',
             'months to train',
             property_type=merlin.ProcessProperty.PropertyType.number_type,
-            default=5,
-            parent=self)
-
-        self.props = {
-            'staff number': p_staff,
-            'staff salary': p_staff_salary,
-            'staff per desk': p_staff_per_desk,
-            'months to train': p_months_to_train}
-
-        self.inputs = {'desks': i_desks, '$': i_funds}
-        self.outputs = {'requests_handled': o_requests_handled}
+            default_value=5)
 
     def reset(self):
         # Calculations that
         # you do not expect to change should be put in the
         # reset function as this is called only once at the
-        # start of the simualtion run.
-        self.desks_required = (
-            self.props['staff number'].get_value() /
-            self.props['staff per desk'].get_value())
-        self.funds_required = (
-            self.props['staff number'].get_value() *
-            self.props['staff salary'].get_value())
-        self.maximal_output = self.props['staff number'].get_value()
-        self.train_slope = 1.0 / self.props['months to train'].get_value()
+        # start of the simulation run.
+        self.desks_required = (self.get_prop_value('staff number') /
+                               self.get_prop_value('staff per desk'))
+        self.funds_required = (self.get_prop_value('staff number') *
+                               self.get_prop_value('staff salary'))
+        self.maximal_output = self.get_prop_value('staff number')
 
     def compute(self, tick):
         # check requirements
-        if self.inputs['desks'].connector.value < self.desks_required:
+        if self.get_input_available('desks') < self.desks_required:
             raise merlin.InputRequirementException(
                 self,
                 self.inputs['desks'],
                 self.inputs['desks'].connector.value,
                 self.desks_required)
-        if self.inputs['$'].connector.value < self.funds_required:
+        if self.get_input_available('$') < self.funds_required:
             raise merlin.InputRequirementException(
                 self,
                 self.inputs['$'],
@@ -115,14 +103,14 @@ class CallCenterStaffProcess(merlin.Process):
 
         # compute outputs
         output = self.maximal_output * self._train_modifier(tick)
-        self.inputs['desks'].consume(self.desks_required)
-        self.inputs['$'].consume(self.funds_required)
-        self.outputs['requests_handled'].connector.write(output)
+        self.consume_input('desks', self.desks_required)
+        self.consume_input('$', self.funds_required)
+        self.provide_output('requests_handled', output)
 
     def _train_modifier(self, tick):
         # This is just a linear function with the
         # slope steepness = months to train
-        mtt = self.props['months to train'].get_value()
+        mtt = self.get_prop_value('months to train')
         train_slope = 1.0 / float(mtt)
         if tick < mtt:
             return tick * train_slope
@@ -136,29 +124,24 @@ class BuildingMaintainenceProcess(merlin.Process):
 
         # Define inputs
         i_funds = merlin.ProcessInput('i_$', '$', connector=None)
+        self.inputs = {'$': i_funds}
 
         # Define outputs
         o_desks = merlin.ProcessOutput('o_desks', 'desks', connector=None)
+        self.outputs = {'desks': o_desks}
 
         # Define properties
-        p_maintenance_cost = merlin.ProcessProperty(
+        self.add_property(
+            'monthly maintenance cost',
             'monthly maintenance cost',
             property_type=merlin.ProcessProperty.PropertyType.number_type,
-            default=500.00,
-            parent=self)
+            default_value=500.00)
 
-        p_desks_provided = merlin.ProcessProperty(
+        self.add_property(
+            'desks provided',
             'desks provided',
             property_type=merlin.ProcessProperty.PropertyType.number_type,
-            default=100.0,
-            parent=self)
-
-        self.props = {
-            'monthly maintenance cost': p_maintenance_cost,
-            'desks provided': p_desks_provided}
-
-        self.inputs = {'$': i_funds}
-        self.outputs = {'desks': o_desks}
+            default_value=100.0)
 
     def reset(self):
         pass
@@ -167,8 +150,8 @@ class BuildingMaintainenceProcess(merlin.Process):
         # Check requirements
         # logging.debug(self.inputs['$'].connector)
         # logging.debug(self.inputs['$'].connector.value)
-        if (self.inputs['$'].connector.value <
-                self.props['monthly maintenance cost'].get_value()):
+        if (self.get_input_available('$') <
+                self.get_prop_value('monthly maintenance cost')):
             raise merlin.InputRequirementException(
                 self,
                 self.inputs['$'],
@@ -176,7 +159,7 @@ class BuildingMaintainenceProcess(merlin.Process):
                 self.props['monthly maintenance cost'].get_value())
 
         # Compute outputs
-        self.inputs['$'].consume(
-                        self.props['monthly maintenance cost'].get_value())
-        self.outputs['desks'].connector.write(
-                        self.props['desks provided'].get_value())
+        self.consume_input('$',
+                           self.get_prop_value('monthly maintenance cost'))
+        self.provide_output('desks',
+                            self.get_prop_value('desks provided'))
